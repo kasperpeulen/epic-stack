c17ea41cfcb4d7886f5180d525df3781c38eea7a
Kasper Peulen <kasperpeulen@gmail.com>
2025-12-22 18:22:30 +0100

    Add migration

---
+++ prisma/migrations/20251222172039_add_kasper_admin_user/migration.sql
@@ -0,0 +1,4 @@
+INSERT INTO User VALUES('cmj4jv90y002187tfm655yfq2','kasperpeulen@gmail.com','admin','Kasper Peulen',1765645709938,1765645709938);
+INSERT INTO Password VALUES('$2b$10$GkPHGuhnQ1UM3KSz1YJ12urNLk55g579V.ImQSD8pLx8C4t5HIWae','cmj4jv90y002187tfm655yfq2');
+INSERT INTO _RoleToUser VALUES('clnf2zvlw000gpcour6dyyuh6','cmj4jv90y002187tfm655yfq2');
+INSERT INTO _RoleToUser VALUES('clnf2zvlx000hpcou5dfrbegs','cmj4jv90y002187tfm655yfq2');
